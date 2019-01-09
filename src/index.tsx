8a393aa0bd4fb613836a76a44ae47db22473a713
tamakiii <tamakiii@users.noreply.github.com>
2019-01-09 16:27:17 +0900

    using hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>hello</div>
-    )
-  }
+function App() {
+  const [value, setValue] = useState(0);
+  const onClick = useCallback(event => setValue(s => s + 1), []);
+  return (
+    <>
+      <div>yo: {value}</div>
+      <button onClick={onClick}>+1</button>
+    </>
+  )
 }
 
 // Render your table
